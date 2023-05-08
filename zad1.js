cc68e32fd06e635ae1f6f7b54dbcb629ac688de2
Kacper Kozlowski <kacperkozlow10@gmail.com>
2023-05-08 19:05:54 +0200

    zadanie pierwsze z powtorki - poprawka bo nie byl js

---
+++ zad1.js
@@ -0,0 +1,7 @@
+function transformToObjects(numberArray) {
+    // Todo: Add your logic
+    // should return an array of objects
+        return numberArray.map(liczba => {
+           return {val: liczba} 
+    });
+}
