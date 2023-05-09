4ca6f699d1a447cd2a8d14c557105eaa20be3281
Kacper Kozlowski <kacperkozlow10@gmail.com>
2023-05-09 12:45:15 +0200

    Drugie zadanie z kursu

---
+++ zad2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// don't change the Component name "App"
+export default function App() {
+    return (
+        <div>
+            <p>Practicing React...</p>
+            <h1>Exercise done!</h1>
+        </div>
+    );
+}
