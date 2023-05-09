d0b2c33565c3b9ada7229313c646a93d3709ce2a
Kacper Kozlowski <kacperkozlow10@gmail.com>
2023-05-09 14:25:10 +0200

    Dodanie css do komponentu

--- p1/01-starting-setup/src/components/ExpenseItem.js
+++ p1/01-starting-setup/src/components/ExpenseItem.js
@@ -1,4 +1,14 @@
+import './ExpenseItem.css';
+
 function ExpenseItem (){
-    return <h2>Expense item!</h2>;
+    return (
+        <div className="expense-item">
+            <div>March 28th 2021</div>
+            <div className="expense-item__description">
+                <h2>Car insurance</h2>
+                <div className="expense-item__price">$294.67</div>
+            </div>
+        </div>
+    );
 }
 export default ExpenseItem;
