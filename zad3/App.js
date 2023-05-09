ab50cfeff4ab819f31a133ea34e923757edb0692
Kacper Kozlowski <kacperkozlow10@gmail.com>
2023-05-09 13:05:43 +0200

    Zadanie 3 z kursu

---
+++ zad3/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Compo from './ExerciseComponent';
+
+// don't change the Component name "App"
+export default function App() {
+    return <Compo/>
+}
---
+++ zad3/ExerciseComponent.js
@@ -0,0 +1,6 @@
+import React from 'react';
+// Todo: Add your component code
+// Important: Use export default function MyCmp() { ... }, don't define "export default" on a separate line!
+export default function Compo (){
+return <p>First exercise - done!</p>;
+}
