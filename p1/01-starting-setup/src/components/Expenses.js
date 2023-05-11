9a313494cc64feffda8d990c4c57099b0fc47207
Kacper Kozlowski <kacperkozlow10@gmail.com>
2023-05-11 14:06:43 +0200

    Modyfikacja stylow css danych obiektow na stronie przez card js

--- p1/01-starting-setup/src/components/Expenses.js
+++ p1/01-starting-setup/src/components/Expenses.js
@@ -1,14 +1,18 @@
 import ExpenseItem from "./ExpenseItem.js";
 import './Expenses.css';
-
+import Card from "./Card";
 
 function Expenses(props){
-    return<div className="expenses">
-      <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
+    return<Card className="expenses">
+      <ExpenseItem
+       title={props.items[0].title}
+       amount={props.items[0].amount}
+       date={props.items[0].date}>
+      </ExpenseItem>
       <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
       <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}></ExpenseItem>
       <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}></ExpenseItem>
 
-    </div>
+    </Card>
 }
 export default Expenses;
